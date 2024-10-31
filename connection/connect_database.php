70818961a95ca34d36e9bf05f4afa3e346f0063e
sandeepal513 <sandeepal513@gmail.com>
2024-10-31 23:41:22 +0530

    Add database connection file and create connection

---
+++ connection/connect_database.php
@@ -0,0 +1,11 @@
+<?php
+
+    $conn = mysqli_connect('localhost','root','','fot_lms');
+
+    if ($conn ->connect_error) {
+        die("Connection  failed: " . $conn->connect_error);
+        exit();
+    }
+    echo "Connect Success";
+
+?>
--- sql_file/sql_databsae.sql
+++ sql_file/sql_databsae.sql
@@ -1,4 +1,3 @@
---Create user table
 CREATE TABLE user (
     user_id INT PRIMARY KEY ,
     username VARCHAR (255) NOT NULL,
@@ -13,10 +12,8 @@ CREATE TABLE user (
     CHECK   (LENGTH(password) >= 8 AND  LENGTH(password) <= 16)
 );
 
---Add new column called user_status to user table
 ALTER TABLE user ADD COLUMN user_status VARCHAR (20);
 
---Create admin table
 CREATE TABLE admin (
     adm_id CHAR (6) primary KEY,
     user_id INT,
@@ -31,7 +28,6 @@ CREATE TABLE admin (
     FOREIGN  KEY (user_id) REFERENCES user(user_id)
 );
 
---Create student table
 CREATE TABLE student (
     stu_id CHAR(6) PRIMARY KEY,
     user_id INT,
@@ -46,7 +42,6 @@ CREATE TABLE student (
     FOREIGN KEY (user_id) REFERENCES user(user_id)
 );
 
---Create lecturer table
 CREATE TABLE lecturer (
     lect_id CHAR(6) PRIMARY KEY,
     user_id INT,
@@ -61,13 +56,11 @@ CREATE TABLE lecturer (
     FOREIGN KEY (user_id) REFERENCES user(user_id)
 );
 
---Create course table
 CREATE TABLE course (
     cour_code  CHAR (6) PRIMARY KEY,
     cour_name VARCHAR (50) NOT NULL
 );
 
---Create stu_course table
 CREATE TABLE stu_course (
     cour_code  CHAR (6),
     stu_id CHAR (6),
@@ -75,7 +68,6 @@ CREATE TABLE stu_course (
     FOREIGN KEY (stu_id) REFERENCES student(stu_id)
 );
 
---Create lect_course table
 CREATE TABLE lect_course (
     cour_code  CHAR (6),
     lect_id CHAR (6),
@@ -83,16 +75,14 @@ CREATE TABLE lect_course (
     FOREIGN KEY (lect_id) REFERENCES lecturer(lect_id)
 );
 
---Insert data into  user table and admin table
-INSERT INTO user
-VALUES 
-(1, 'admin', 'Sarath', 'Fernando', 'admin123@gmail.com', 'Admin200@', '0766816285', '1980-02-05', 'male');
+INSERT INTO user(user_id, username, first_name,  last_name, email, password, mb_no, dob, gender)
+VALUES
+(1, 'admin', 'Sarath', 'Fernando', 'admin123@gmail.com', 'Admin200@', '0766816285', '1980-02-05', 'male'),
 (2, 'sandeepal', 'sandeepa', 'lakshan', 'lsandeepa13@gmail.com', 'Sandeepa@#2002', '0766816272', '2002-02-05', 'male'),
 (3, 'nethmin', 'nethmi', 'nimasha', 'nethminimasha@gmail.com', 'Nethmi@200#', '0764245185', '2002-02-07', 'female'),
 (4, 'kasuns', 'kasun', 'samarakoon', 'kasunsama@gmail.com', 'Kasun@1998$', '0712345678', '1998-05-12', 'male'),
 (5, 'sachinir', 'sachini', 'rathnayake', 'sachirathna@gmail.com', 'Sachini#2020', '0719876543', '2000-11-25', 'female');
 
---Insert data to user_status column in  user table
 UPDATE user
 SET user_status = CASE user_id
     WHEN 1 THEN 'admin'
@@ -103,25 +93,20 @@ SET user_status = CASE user_id
 END
 WHERE user_id IN (1, 2, 3, 4, 5);
 
-
 INSERT INTO admin
 VALUES
 ('a001',1,'admin','Sarath','Fernando','admin123@gmail.com','Admin200@','0766816285','1980-02-05','male');
 
---Insert student data into  student table
 INSERT INTO student
 VALUES
 ('s001',2,'sandeepal','sandeepa','lakshan','lsandeepa13@gmail.com','Sandeepa@#2002','0766816272','2002-02-05','male'),
 ('s002',3,'nethmin','nethmi','nimasha','nethminimasha@gmail.com','Nethmi@200#','0764245185','2002-02-07','female');
 
---Insert lecturer data into lecturer table
 INSERT  INTO lecturer
 VALUES
 ('l001', 4, 'kasuns', 'kasun', 'samarakoon', 'kasunsama@gmail.com', 'Kasun@1998$', '0712345678', '1998-05-12', 'male'),
 ('l002', 5, 'sachinir', 'sachini', 'rathnayake', 'sachirathna@gmail.com', 'Sachini#2020', '0719876543', '2000-11-25', 'female');
 
-
---Insert course data into course table
 INSERT INTO course
 VALUES
 ('c001', 'Data Structures'),
