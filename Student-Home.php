ef758fe9d0cebbd87fe38c594e3c27ba8a319da9
Anjana-Nirmal <anjanan414@gmail.com>
2024-11-02 19:27:27 +0530

    Created the profile.php file.

--- Student-Home.php
+++ Student-Home.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="css/style2.css">
-    <link rel="stylesheet" href="./css/view.css">
+    <link rel="stylesheet" href="css/view.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <script defer src="bootstrap.min.js"></script>
