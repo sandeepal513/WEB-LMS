ab7e4fe218175bc74a5db583c7ede414450fdee8
Anjana-Nirmal <anjanan414@gmail.com>
2024-11-02 12:47:11 +0530

    Added new files.

---
+++ Student-Home.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style2.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+</head>
+<body>
+    
+    <!--Sidebar-->
+    <div class="sidebar d-flex flex-column align-items-center">
+        <div class="profile">
+            <img src="./img/lecturer_profile.png" alt="Profile" class="profile-pic">
+            <h2>Lecturer Name</h2>
+            <h6 class="stu-name">LECTURER</h6>
+            <button class="btn btn-primary">View Profile</button>
+        </div>
+        <ul class="nav flex-column nav-links w-100 px-3">
+            <li class="nav-item active">
+                <a href="#" class="nav-link">Home</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">View Courses</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">Register Course</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">Contact Us</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">Logout</a>
+            </li>
+        </ul>
+    </div>
+
+    <!--Head Content-->
+    <div class="head-content">
+        <header class="d-flex justify-content-center align-content-center mb-4">
+            <input type="text" placeholder="Serach Courses...." class="form-control w-25">
+        </header>
+        <div class="main-content">
+            <!--Your Content-->
+        </div>
+    </div>
+</body>
+</html>
